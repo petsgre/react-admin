55e50fec13c449a803fbea784dee996088f45158
张啸 <petsgre@gmail.com>
2020-01-26 21:57:42 +0800

    feat:add NoPermission page

---
+++ src/pages/NoPermission.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+class NoPermission extends React.Component<any, any> {
+  public render() {
+    return <div className="">NoPermission</div>;
+  }
+}
+
+export default NoPermission;
