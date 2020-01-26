be89c83413badb3b2048321137499d573a2597c1
张啸 <petsgre@gmail.com>
2020-01-26 20:49:19 +0800

    feat:add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" component={Dashboard} />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
