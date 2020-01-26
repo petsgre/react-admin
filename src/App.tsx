2064fe6fa97b9e297b50d653669d5ee64a8321de
张啸 <petsgre@gmail.com>
2020-01-26 21:25:15 +0800

    feat:add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./App.css";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+import Container from "@/pages/Container";
+import Login from "@/pages/Login";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/" component={Dashboard} />
+          <Route path="/Login" component={Login} />
+          <Route path="/" component={Container} />
         </Switch>
       </Router>
     </div>
